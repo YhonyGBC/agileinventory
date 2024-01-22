2965ace9257bfc1a64ac4e89747abbe469677d7a
Gabriel Benavides <yhonygbc@gmail.com>
2024-01-22 15:52:14 -0500

    primer commit

---
+++ src/main/java/com/mycompany/agileinventory/Agileinventory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agileinventory;
+
+/**
+ *
+ * @author ttaniel
+ */
+public class Agileinventory {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
