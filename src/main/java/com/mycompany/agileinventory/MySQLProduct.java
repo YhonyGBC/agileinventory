105997c5c3c039be1aa3e3a7bbf3d7c6a2594ed6
alejo-c <4lejo.castrillon@gmail.com>
2024-01-23 15:23:36 -0500

    Hacer que las clases concretas de un producto hereden la clase abstracta

--- src/main/java/com/mycompany/agileinventory/MySQLProduct.java
+++ src/main/java/com/mycompany/agileinventory/MySQLProduct.java
@@ -1,59 +1,9 @@
 package com.mycompany.agileinventory;
 
-public class MySQLProduct implements IProduct {
-
-    private int id;
-    private String name;
-    private int quantity;
-    private float pricePerUnit;
-    private final String DBMS;
+public class MySQLProduct extends AbstractProduct {
 
     public MySQLProduct(int id, String name, int quantity, float pricePerUnit) {
-        this.id = id;
-        this.name = name;
-        this.quantity = quantity;
-        this.pricePerUnit = pricePerUnit;
+        super(id, name, quantity, pricePerUnit);
         this.DBMS = "MySQL";
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getQuantity() {
-        return this.quantity;
-    }
-
-    public float getPricePerUnit() {
-        return this.pricePerUnit;
-    }
-
-    public String getDBMS() {
-        return this.DBMS;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setPricePerUnit(float pricePerUnit) {
-        this.pricePerUnit = pricePerUnit;
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
     }
 }
--- src/main/java/com/mycompany/agileinventory/PostgreSQLProduct.java
+++ src/main/java/com/mycompany/agileinventory/PostgreSQLProduct.java
@@ -1,59 +1,9 @@
 package com.mycompany.agileinventory;
 
-public class PostgreSQLProduct implements IProduct {
-
-    private int id;
-    private String name;
-    private int quantity;
-    private float pricePerUnit;
-    private final String DBMS;
+public class PostgreSQLProduct extends AbstractProduct {
 
     public PostgreSQLProduct(int id, String name, int quantity, float pricePerUnit) {
-        this.id = id;
-        this.name = name;
-        this.quantity = quantity;
-        this.pricePerUnit = pricePerUnit;
-        this.DBMS = "PosgreSQL";
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getQuantity() {
-        return this.quantity;
-    }
-
-    public float getPricePerUnit() {
-        return this.pricePerUnit;
-    }
-
-    public String getDBMS() {
-        return this.DBMS;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setPricePerUnit(float pricePerUnit) {
-        this.pricePerUnit= pricePerUnit;
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
+        super(id, name, quantity, pricePerUnit);
+        this.DBMS = "PostgreSQL";
     }
 }
