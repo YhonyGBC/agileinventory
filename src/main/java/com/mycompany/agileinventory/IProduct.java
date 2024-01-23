06d127150e4b0014ede1068c8e116067639567df
alejo-c <4lejo.castrillon@gmail.com>
2024-01-23 15:19:55 -0500

    Cambiar los parametros de algunos métodos
    Para el método setQuantity, se le cambio el nombre del parametro count a quantity.
    
    Para el méetodo setPricePerUnit, se le cambio el nombre del parametro unitValue a pricePerUnit.

--- src/main/java/com/mycompany/agileinventory/IProduct.java
+++ src/main/java/com/mycompany/agileinventory/IProduct.java
@@ -2,13 +2,20 @@ package com.mycompany.agileinventory;
 
 public interface IProduct extends Cloneable {
     public int getId();
+
     public String getName();
+
     public int getQuantity();
+
     public float getPricePerUnit();
+
     public String getDBMS();
 
     public void setId(int id);
+
     public void setName(String name);
-    public void setQuantity(int count);
-    public void setPricePerUnit(float unitValue);
+
+    public void setQuantity(int quantity);
+
+    public void setPricePerUnit(float pricePerUnit);
 }
